766ea256a3f88c305341fe6663a967c3e1b03166
Muller Esposito <mulleresposito@hotmail.com>
2021-02-26 23:37:25 -0300

    feat(handleOnChange): has been created the handle function to convert the form elements in controlled components;

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -7,6 +7,20 @@ import RadioGroup from '../RadioGroup/RadioGroup';
 import './style.css'
 
 class Form extends Component {
+    constructor(){
+        super();
+        this.state = {
+        }
+        
+    }
+
+    handleOnChange = ({ target : { name, value } }) => {
+        console.log(`handlOnChange...`);
+        this.setState({
+            [name]: value
+        })
+    }
+
     render() {
         const options = [
             { label: "SELECT STAGE", value: "" },
@@ -19,24 +33,27 @@ class Form extends Component {
             { id: "radio-casa", name: "tipo", value: "Casa", label: "Casa:"},
             { id: "radio-apartamento", name: "tipo", value: "Apartamento", label: "Apartamento:"}
         ]
+        const { 
+                inputName, inputEmail, inputCpf, inputCity, inputRoleDesc,
+                textareaAddress, selectStates, textareaResume, textareaRole  } = this.state;
 
         return (
            <form>
                 <fieldset>
                     <legend>Personal Data</legend>
-                    <Input id="input-name" type="text" max="40" required={true}>Name:</Input>
-                    <Input id="input-email" type="text" max="50" required={true}>Email:</Input>
-                    <Input id="input-cpf" type="text" max="11" required={true}>CPF:</Input>
-                    <Input id="textarea-address" type="textarea" max="200" required={true}>Address:</Input>
-                    <Input id="input-city" type="text" max="28" required={true}>City:</Input>
-                    <Select id="select-states" name="select-states" listOptions={options}>State:</Select>
-                    <RadioGroup listRadio={radios}>Tipo:</RadioGroup>
+                    <Input id="inputName" type="text" max="40" required={true} value={inputName} handleOnChange={this.handleOnChange} >Name:</Input>
+                    <Input id="inputEmail" type="text" max="50" required={true} value={inputEmail} handleOnChange={this.handleOnChange}>Email:</Input>
+                    <Input id="inputCpf" type="text" max="11" required={true} value={inputCpf} handleOnChange={this.handleOnChange}>CPF:</Input>
+                    <Input id="textareaAddress" type="textarea" max="200" required={true} value={textareaAddress} handleOnChange={this.handleOnChange}>Address:</Input>
+                    <Input id="inputCity" type="text" max="28" required={true} value={inputCity} handleOnChange={this.handleOnChange}>City:</Input>
+                    <Select id="selectStates" name="select-states" listOptions={options} handleOnChange={this.handleOnChange}>State:</Select>
+                    <RadioGroup listRadio={radios} handleOnChange={this.handleOnChange}>Tipo:</RadioGroup>
                 </fieldset>
                 <fieldset>
                     <legend>Last Job Data</legend>
-                    <Input id="textarea-resume" type="textarea" max="1000" required={true}>Resume></Input>
-                    <Input id="textarea-role" type="textarea" max="40" required={true}>Role:</Input>
-                    <Input id="input-role-description" type="text" max="500" required={true}>Role Description:</Input>
+                    <Input id="textareaResume" type="textarea" max="1000" required={true} value={textareaResume} handleOnChange={this.handleOnChange}>Resume</Input>
+                    <Input id="textareaRole" type="textarea" max="40" required={true} value={textareaRole} handleOnChange={this.handleOnChange}>Role:</Input>
+                    <Input id="inputRoleDesc" type="text" max="500" required={true} value={inputRoleDesc} handleOnChange={this.handleOnChange}>Role Description:</Input>
                 </fieldset>
            </form>
         );
