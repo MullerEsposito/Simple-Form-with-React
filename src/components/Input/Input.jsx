ec015951cf10ee30621b27a0014fa7f387e0955c
Muller Esposito <mulleresposito@hotmail.com>
2021-02-26 23:44:48 -0300

    feat: transforms this component in a controlled component;

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -4,19 +4,26 @@ import './style.css';
 
 class Input extends Component {
     render() {
-        const { id, type, max, required, children } = this.props;
-
+        const { id, type, max, required, children, handleOnChange, value } = this.props;
+        
         return (
             <div className="container-input">
                 <label htmlFor={id}>
                     {children}
                 </label>
                     {(()=> {
-                        if (type === 'text') {
-                            return <input id={id} type={type} maxLength={max} required={required}/>;
-                        } else if (type === 'textarea') {
-                            return <textarea id={id} maxLength={max} required={required}/>
-                        }
+                        if (type === 'textarea') return (
+                            <textarea 
+                                id={id} name={id} value={value} maxLength={max}
+                                required={required} onChange={handleOnChange}
+                            />
+                        ) 
+                        
+                        return <input 
+                                    id={id} name={id} type={type} 
+                                    maxLength={max} required={required}
+                                    onChange={handleOnChange} value={value}
+                                />;
                     })()}
             </div>
         );
